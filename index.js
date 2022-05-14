7c6b785b77dcf75c5d579e1393b5b2dbf146b889
mriduldas07 <mriduldas0325@gmail.com>
2022-05-14 16:20:53 +0600

    final

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(express.json());
 
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ivgpu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
